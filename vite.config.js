fb12f22fce9411e3a4ef0542053e99d91ed52902
Hardhik <hardhik2017@gmail.com>
2024-10-24 16:59:31 -0400

    Update vite.config.js and fix Netlify build issue

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,35 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default {
+//   build: {
+//     rollupOptions: {
+//       external: ['react-scroll'],
+//     },
+//   },
+// }; 
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+// })
+
+
+// Import necessary packages
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Merge all configurations into a single export
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000,  // Define the port for local development
   },
-})
+});
