79fec0382ec5f0291ccf1eb0cd39a48d5a110ad5
Hardhik <athardhik06@gmail.com>
2025-02-05 00:51:03 -0500

    reverted

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001F3F', // Define navy color
+      },
+    },
+  },
+  plugins: [],
+};
+
+
