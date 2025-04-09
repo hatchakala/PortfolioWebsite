f97907b1e8d9114e0a6f16dbaf9a8d746308c85b
Hardhik <hardhik2017@gmail.com>
2025-04-09 15:55:08 -0400

    fixed borders

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,52 @@
+// import About from "./components/About";
+// import Contact from "./components/Contact";
+// import Experience from "./components/Experience";
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Projects from "./components/Projects";
+// import Technologies from "./components/Technologies";
+
+// const App = () => {
+//   return (
+//     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+//       <div className="fixed top-0 -z-10 h-full w-full">
+//         <div className="absolute top-0 z-[-2] h-screen w-screen bg-navy"></div>
+//       </div>
+
+//       <div className="container mx-auto px-8">
+//         <Navbar />
+        
+//         {/* Hero section with blue border below */}
+//         <div className="border-b-4 border-[#2965F1] pb-24">
+//           <Hero />
+//         </div>
+
+//         {/* Adjusted About section without border */}
+//         <div className="pb-16">
+//           <About />
+//         </div>
+
+//         {/* Technologies section without extra border */}
+//         <Technologies />
+
+//         {/* Experience section with blue border below */}
+//         <div className="border-b-4 border-[#2965F1] pb-24">
+//           <Experience />
+//         </div>
+
+//         {/* Projects section with blue border below */}
+//         <div className="border-b-4 border-[#2965F1] pb-24">
+//           <Projects />
+//         </div>
+
+//         <Contact />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
@@ -17,7 +66,7 @@ const App = () => {
         <Navbar />
         
         {/* Hero section with blue border below */}
-        <div className="border-b-4 border-[#2965F1] pb-24">
+        <div className="border-b-2 border-[#2965F1] pb-24">
           <Hero />
         </div>
 
@@ -30,12 +79,12 @@ const App = () => {
         <Technologies />
 
         {/* Experience section with blue border below */}
-        <div className="border-b-4 border-[#2965F1] pb-24">
+        <div className="border-b-2 border-[#2965F1] pb-24">
           <Experience />
         </div>
 
         {/* Projects section with blue border below */}
-        <div className="border-b-4 border-[#2965F1] pb-24">
+        <div className="border-b-2 border-[#2965F1] pb-24">
           <Projects />
         </div>
 
@@ -46,6 +95,7 @@ const App = () => {
 };
 
 export default App;
+
 
 
 
