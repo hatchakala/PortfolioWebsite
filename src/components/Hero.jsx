06b3490436edd0fd560c02a6baf8df6696b46387
Hardhik <hardhik2017@gmail.com>
2025-09-17 00:45:16 -0400

    fixed picture error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/HardhikMainWebsitePFPNoBackground.jpg";
+import profilePic from "../assets/HardhikMainWebsitePFP.jpg";
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import { useEffect, useState } from "react";
